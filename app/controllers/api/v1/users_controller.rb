5e40196ad84374f4c2276419dc6401140f8197df
Moussa Essalhi <essalhi.me01@gmail.com>
2023-04-30 17:27:14 +0100

    create table users and add functions and create raoute

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,72 @@
 class Api::V1::UsersController < ApplicationController
+    before_action :getUser, only: [:deleteUser, :updateUser, :show]
+    #get
+    def getUsers
+        user = User.all
+        if user
+            render json: user, status: :ok
+        else
+            render json: {msg: "user empty"}, status: :unprocessable_entity
+        end
+    end
+
+    #post
+    def addUsers
+        user = User.new(username: params[:username], password_digest: params[:password], email: params[:email])
+        if user.save()
+            render json: user, status: :ok
+        else
+            # render json: {message: "User not added", status: :unprocessable_entity}
+            render json: {message: "User not added"}, status: :unprocessable_entity
+            
+        end
+        
+    end
+
+    #show
+    def show
+        if  @user   
+            render json: @user, status: :ok
+           
+        else
+            render json: {message: "User not Found"}, status: :unprocessable_entity
+        end
+    end
+
+    #put
+    def updateUser
+        if  @user
+            if  @user.update(userparams)
+                render json: @user, status: :ok
+            else
+                render json: {message: "Update Failed"}, status: :unprocessable_entity
+            end
+        else
+            render json: {message: "User not Found"}, status: :unprocessable_entity
+        end
+    end
+
+    #delete
+    def deleteUser
+        if  @user
+            if  @user.destroy(userparams)
+                render json: {message: "User deleted"}, status: :ok
+            else
+                render json: {message: "delete Failed"}, status: :unprocessable_entity
+            end
+        else
+            render json: {message: "User not Found"}, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def userparams
+            params.permit(:username, :password_digest, :email);
+            
+        end
+
+        def getUser
+            @user = User.find(params[:id])
+            
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  namespace :api do 
+    namespace :v1 do
+      get 'get_user', action: :getUsers, controller: :users
+      post 'add_user', action: :addUsers, controller: :users
+      get 'show_user', action: :show, controller: :users
+      put 'update_user', action: :updateUser, controller: :users
+      delete 'delet_user', action: :deleteUser, controller: :users
+    end
+  end
 end
